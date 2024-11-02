df2152472df64f898b641a451a2b38fe019643aa
aescbr <aescobar@applaudostudios.com>
2024-11-01 23:35:29 -0600

    try injector

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 from service.pre_processing_service import PreProcessingService
 from model.request import Request
-
+from strategy.handler.auth_verification_handler import AuthVerificationhandler
+from strategy.handler.source_validation_handler import SourceValidationHandler
+from strategy.handler.file_extension_validation_handler import FileExtensionValidation
 
 
 def main() -> int:
-    service = PreProcessingService();
+    authVerificationHandler = AuthVerificationhandler()
+    sourceValidationHandler = SourceValidationHandler()
+    fileExtensionValidation = FileExtensionValidation()
+
+    service = PreProcessingService(authVerificationHandler, sourceValidationHandler, fileExtensionValidation);
 
     request = Request("request1", ["path1", "path2"])
     service.process(request)
--- src/service/pre_processing_service.py
+++ src/service/pre_processing_service.py
@@ -2,20 +2,25 @@ from model.request import Request
 from strategy.handler.auth_verification_handler import AuthVerificationhandler
 from strategy.handler.source_validation_handler import SourceValidationHandler
 from strategy.handler.file_extension_validation_handler import FileExtensionValidation
+from injector import inject 
 
-class PreProcessingService():
+class PreProcessingService:
+
+    @inject
+    def __init__(self, authValidationHandler: AuthVerificationhandler, 
+                 sourceValidationHandler: SourceValidationHandler, 
+                 fileExtensionValidation: FileExtensionValidation):
+        self.authValidationHandler = authValidationHandler
+        self.sourceValidationHandler = sourceValidationHandler
+        self.fileExtensionValidation = fileExtensionValidation
 
     def process(self, request: Request):
         """
         A service to start the chain
         """
-        authHandler = AuthVerificationhandler();
-        sourceValidation = SourceValidationHandler();
-        fileExtensionValidation = FileExtensionValidation();
+        self.authValidationHandler.set_next(self.sourceValidationHandler).set_next(self.fileExtensionValidation)
 
-        authHandler.set_next(sourceValidation).set_next(fileExtensionValidation)
-
-        result = authHandler.handle(request)
+        result = self.authValidationHandler.handle(request)
         print(result)
     
     
